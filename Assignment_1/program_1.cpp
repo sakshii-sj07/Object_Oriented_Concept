98c7159d7c4a65c32c90f2c6329cf43a5ac48b82
SAKSHI JADHAV <sakshijadhav8856@gmail.com>
2025-10-17 00:59:10 +0530

    Add files via upload

---
+++ Assignment_1/program_1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Student {
+    private:
+        char name[30];
+        int rollNo;
+        float marks;
+
+    public:
+    
+    // Member function to input student details.
+    void inputDetails() {
+        cout<<"Enter student name : ";
+        cin>>name;
+        cout<<"Enter roll number : ";
+        cin>>rollNo;
+        cout<<"Enter marks : ";
+        cin>>marks;
+    }
+
+    // Member function to display student details
+    void displayDetails() {
+        cout<<"Student Name : "<<name<<endl;
+        cout<<"Roll Number : "<<rollNo<<endl;
+        cout<<"Marks : "<<marks<<endl;
+    }
+};
+
+int main() {
+    Student studentObj;
+
+    // Input student details
+    studentObj.inputDetails();
+
+    // Display student details
+    cout<<"\nStudent Details\n";
+    studentObj.displayDetails();
+
+    return 0;
+}
