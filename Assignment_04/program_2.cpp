c4741493d3e10bdb62df139629dccfdd3f721fab
sakshii-sj07 <sakshijadhav8856@gmail.com>
2025-11-01 12:08:47 +0530

    Assignment 4 program 2

---
+++ Assignment_04/program_2.cpp
@@ -0,0 +1,44 @@
+// C++ program to illustrate Constructor overloading
+
+#include <iostream>
+using namespace std;
+
+class Construct
+{
+public:
+    float area;
+
+    // Constructor with no parameters
+    Construct()
+    {
+        area = 0;
+    }
+
+    // Constructor with two parameters
+    Construct(int a, int b)
+    {
+        area = a * b;
+    }
+
+    void Display()
+    {
+        cout << area << endl;
+    }
+};
+
+int main()
+{
+    cout << "Name : Sakshi Yogesh Jadhav." << endl;
+    cout << "Class : SY-B." << endl;
+    cout << "Roll No. : 84." << endl;
+    cout << endl;
+
+    // Constructor overloading with two different constructors of clss name
+    Construct O1;
+    Construct O2(10, 20);
+
+    O1.Display();
+    O2.Display();
+
+    return 0;
+}
