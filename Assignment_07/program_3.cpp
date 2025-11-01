3034f035125e84ea1620b3dcd6ee2e52e3eb697d
sakshii-sj07 <sakshijadhav8856@gmail.com>
2025-11-01 12:18:00 +0530

    Assignment 7 program 3

---
+++ Assignment_07/program_3.cpp
@@ -0,0 +1,56 @@
+// C++program to illustrate the implementation of Hybrid Inheritance
+
+#include <iostream>
+using namespace std;
+
+// base class
+class Vehicle
+{
+public:
+    Vehicle()
+    {
+        cout << "This is a Vehicle" << endl;
+    }
+};
+
+// base class
+class Fare
+{
+public:
+    Fare()
+    {
+        cout << "Fare of Vehicle" << endl;
+    }
+};
+
+// first subclass
+class Car : public Vehicle
+{
+public:
+    Car()
+    {
+        cout << "This Vehicle is Car" << endl;
+    }
+};
+
+// second subclass
+class Bus : public Vehicle, public Fare
+{
+public:
+    Bus()
+    {
+        cout << "This Vehicle is Bus with Fare" << endl;
+    }
+};
+
+int main()
+{
+    cout << "Name : Sakshi Yogesh Jadhav." << endl;
+    cout << "Class : SY-B." << endl;
+    cout << "Roll No. : 84." << endl;
+    cout << endl;
+
+    Bus obj;
+
+    return 0;
+}
